7173ec2787a65c5e5497ab2d35963a2d9756d081
Miron <hamer223@o2.pl>
2019-09-01 20:54:43 +0200

    fixes in MainThread

--- src/Service/MainThread/MainThread.java
+++ src/Service/MainThread/MainThread.java
@@ -1,29 +1,63 @@
 package Service.MainThread;
 
-import Service.Editor.Editor;
-import Service.Helpers.TickHelper;
+import Service.Service;
 
 public class MainThread implements Runnable{
 
-    private Editor editor;
+    private Service service;
     private TickHelper tickHelper;
 
-    public MainThread(Editor editor){
-        this.editor = editor;
+    public MainThread(Service service){
+        this.service = service;
         tickHelper = new TickHelper();
     }
 
     @Override
     public void run() {
-        while(editor.running){
+        while(service.running){
             tickHelper.calcTime();
 
             while(tickHelper.readyToUpdate()){
-                editor.update();
                 tickHelper.update();
+                service.render();
             }
-            editor.render();
+            service.update();
             tickHelper.reset();
         }
     }
+    private class TickHelper {
+        private int frames = 0;
+        private long lastTime = System.nanoTime();
+        private long timer = System.currentTimeMillis();
+        private double delta = 0.0;
+        private double ns = 1000000000.0/60.0;
+        private int updates = 0;
+
+        void calcTime(){
+            long now = System.nanoTime();
+            delta+=(now-lastTime)/ns;
+            lastTime = now;
+            frames++;
+        }
+
+        boolean readyToUpdate() {
+            return delta>=1;
+        }
+
+        void update(){
+            updates++;
+            delta--;
+        }
+
+        void reset(){
+            if(System.currentTimeMillis()-timer>1000){
+                System.out.println("FPS: " + frames + ",\t updates: " + updates);
+                timer+=1000;
+                frames=0;
+                updates=0;
+            }
+        }
+
+    }
+
 }
