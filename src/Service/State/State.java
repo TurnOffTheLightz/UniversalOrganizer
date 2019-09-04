399cb716bb70cad0c6ebd56e0a3c762c23a5709b
Miron <hamer223@o2.pl>
2019-09-04 03:15:52 +0200

    improved state enum

--- src/Service/State/State.java
+++ src/Service/State/State.java
@@ -1,8 +1,28 @@
 package Service.State;
 
-public enum State {
+public enum State {     mainMenu,calculator,tempoDetector,blockDiagram;
 
-    mainMenu,tempoDetector,blockDiagram,calculator;
     public static State state;
 
-    };
+    public static void setStateFromString(String string){
+        if(string.equals("main-menu")) state = mainMenu;
+        else if(string.equals("calculator")) state = calculator;
+        else if(string.equals("tempo-detector")) state = tempoDetector;
+        else if(string.equals("block-diagram")) state = blockDiagram;
+     }
+
+    public static boolean mainMenuOpen(){ return state == mainMenu; }
+
+    public static boolean calculatorOpen(){
+        return state == calculator;
+    }
+
+    public static boolean tempoDetectorOpen(){
+        return state == tempoDetector;
+    }
+
+    public static boolean blockDiagramOpen(){
+        return state == blockDiagram;
+    }
+
+};
