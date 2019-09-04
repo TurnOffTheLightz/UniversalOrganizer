7fc4292a11cc3e2a8a0a47cdfd9ba7e3abe6cafb
Miron <hamer223@o2.pl>
2019-09-04 03:11:30 +0200

    updated frame class

--- src/FrameComponents/Frame/Frame.java
+++ src/FrameComponents/Frame/Frame.java
@@ -1,97 +1,75 @@
 package FrameComponents.Frame;
 
-import Controllers.MouseController;
-import Service.Helpers.ButtonHelper;
-import Service.Helpers.MouseHelper;
+import Listeners.ActionListener;
+import Service.Helpers.MenuBarHelper;
+import Service.Managment.ButtonManager;
+import Service.Helpers.ActionHelper;
 import Service.Managment.ContainerManager;
 import Service.Managment.ImageManager;
 import Service.Managment.LayoutManager;
 import Service.Functionalities.MainMenu.MainMenu;
-import Service.State.State;
 
 import javax.swing.*;
 import java.awt.*;
 
 
 public class Frame extends JFrame {
-
     /*
     Frame class stores data needed to move between cards(functionalities) in apps
 
         maybes:
         -not sure about preinitializing classes with constructor arguments
      */
-
-    //TODO::MouseHelper class, needs buttonHelper
     private final Dimension frameSize = new Dimension(800,800);
 
-    private ContainerManager containerManager;
-    private JPanel baseContainer;
-    private Canvas canvas;
+    private ButtonManager buttonManager = new ButtonManager();
 
-    private ButtonHelper buttonHelper = new ButtonHelper();
+    private ContainerManager containerManager=  new ContainerManager(buttonManager);
 
-    private MouseHelper mouseHelper = new MouseHelper(buttonHelper);
+    private LayoutManager layoutManager = new LayoutManager(containerManager);
 
-    private MainMenu mainMenu;
+    private ActionHelper actionHelper = new ActionHelper(layoutManager, buttonManager);
 
-    private MouseController mouseController;
+    private ActionListener actionListener = new ActionListener(actionHelper);
 
+    private MenuBarHelper menuBarHelper = new MenuBarHelper();
+
+    //TODO:: image manager class
     private ImageManager imageManager = new ImageManager();
 
-    private LayoutManager layoutManager;
-
     public Frame(){
-        initContainerHelper();
-        initCanvas();
-        initMainMenu();
-        initFrame();
-        initMouseListener();
-        createLayoutManager();
+        setFrame();
+        setMenuBar();
+        addContainers();
+        addButtons();
+        addBaseContainerToFrame();
+        addActionListenerToButtons();
     }
 
-    public void render(Graphics g){
-        if(State.state == State.mainMenu){
-            renderBackground(g);
-            buttonHelper.render(g);
-        }
+    public void render(){
+        containerManager.render();
     }
-
-    private void renderBackground(Graphics g){
-        g.setColor(imageManager.getBackgroundColor());
-        g.fillRect(canvas.getX(),canvas.getY(),canvas.getWidth(),canvas.getHeight());
-    }
-    //TODO:: create ButtonHelper
 
     public void update(){
-//        System.out.println(containerManager.getBaseContainer().getLayout());
-        mouseHelper.update();
-        buttonHelper.update();
+
     }
 
-    private void createLayoutManager(){
-        layoutManager = new LayoutManager(containerManager);
+    private void setMenuBar(){
+        this.setJMenuBar(menuBarHelper.getMenuBar());
     }
 
-    private void initContainerHelper() {
-        containerManager = new ContainerManager();
-        setBaseContainer();
+    private void addBaseContainerToFrame(){ this.add(containerManager.getBaseContainer()); }
+
+    private void addButtons(){ layoutManager.addButtons(); }
+
+    private void addContainers(){ containerManager.addContainersToBaseContainer(); }
+
+    private void addActionListenerToButtons(){
+        buttonManager.addActionListener(actionListener);
     }
 
-    private void initCanvas() {
-        canvas = new Canvas();
-//        baseContainer.add(canvas);
-        this.add(canvas);
-    }
-
-    private void initMainMenu(){
-        mainMenu = new MainMenu();
-        this.setJMenuBar(mainMenu.getMenuBar());
-        State.state = State.mainMenu;
-    }
-
-    private void initFrame(){
-        this.setTitle("Universal Organizer");
+    private void setFrame(){
+        this.setTitle("Universal Organizer v0.1");
         this.setSize(frameSize);
         this.setFocusable(true);
         this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
@@ -99,19 +77,4 @@ public class Frame extends JFrame {
         this.setResizable(false);
         this.setVisible(true);
     }
-
-    private void initMouseListener(){
-        mouseController = new MouseController();
-        canvas.addMouseListener(mouseController);
-    }
-
-    private void setBaseContainer(){
-        baseContainer = containerManager.getBaseContainer();
-    }
-
-    public Canvas getCanvas(){
-        return canvas;
-    }
-
-    public MainMenu getMainMenu(){ return mainMenu; }
 }
