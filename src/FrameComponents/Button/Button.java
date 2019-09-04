1e37d0da9d9394cacf8ad211e0a455f1962718f3
Miron <hamer223@o2.pl>
2019-09-04 03:10:47 +0200

    updated extending JButton

--- src/FrameComponents/Button/Button.java
+++ src/FrameComponents/Button/Button.java
@@ -1,81 +1,33 @@
 package FrameComponents.Button;
 
-import Service.State.State;
-
+import javax.swing.*;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 
-public class Button extends java.awt.Button {
+public class Button extends JButton {
     /*
     Button stores information like bounds, text, position, image etc.
      */
     private String text;
-    private Rectangle bounds;
 
     //TODO::add graphics and animations
     private BufferedImage image;
     //meanwhile:
     private Color color;
 
-    private boolean pressed = false;
 
-    public Button(Rectangle bounds,String text, Color color){
-        this.bounds = bounds;
+    public Button(String text, Color color) {
+        super(text);
         this.text = text;
         this.color = color;
     }
 
-    public void render(Graphics g){
-        renderImage(g);
-        changeButtonColor(g);
-        renderText(g);
-    }
-
-    private void changeButtonColor(Graphics g){
-        if(pressed){
-            g.setColor(new Color(0,150,0));
-            g.fillRect(bounds.x,bounds.y,bounds.width,bounds.height);
-        }
-    }
-
-    public Rectangle getBounds(){
-        return bounds;
-    }
-
-    public void setPressed(boolean pressed){
-        this.pressed = pressed;
-    }
-
-    public boolean pressed(){
-        return pressed;
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
     }
 
     public String getText(){
         return text;
-    }
-
-    private void renderImage(Graphics g){
-        g.setColor(color);
-        g.fillRect(bounds.x,bounds.y,bounds.width,bounds.height);
-    }
-    private void renderText(Graphics g){
-        g.setColor(Color.black);
-        g.setFont(new Font("TimesRoman", Font.BOLD, 22));
-        g.drawString(text,getTextPosition().x,getTextPosition().y);
-    }
-    private Point getTextPosition(){
-        return new Point(bounds.x+bounds.width/3,bounds.y+bounds.height/2);
-    }
-
-    public void update() {
-
-    }
-
-    public State getState(){
-        if(text.equals("main-menu")) return State.mainMenu;
-        if(text.equals("tempo-detector")) return State.tempoDetector;
-        if(text.equals("block-diagram")) return State.blockDiagram;
-        if(text.equals("calculator")) return State.calculator;
-        return null;
     }
 }
