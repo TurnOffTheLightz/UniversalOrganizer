a6c5792d9fd4a607f30b64e653c1bc52c50b1375
Miron <hamer223@o2.pl>
2019-09-04 03:11:02 +0200

    added container class

---
+++ src/FrameComponents/Container/Container.java
@@ -0,0 +1,16 @@
+package FrameComponents.Container;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Container extends JPanel {
+
+    public Container(){
+
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+    }
+}
