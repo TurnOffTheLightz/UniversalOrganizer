91f769fbab7f99323bbb697c487f32c57ca19c4a
Miron <hamer223@o2.pl>
2019-09-04 03:12:04 +0200

    to do in future

--- src/Service/Functionalities/MainMenu/MainMenu.java
+++ src/Service/Functionalities/MainMenu/MainMenu.java
@@ -1,24 +1,7 @@
 package Service.Functionalities.MainMenu;
 
-import Service.Helpers.MenuBarHelper;
-
-import javax.swing.*;
 
 public class MainMenu {
-
-
-    private MenuBarHelper menuBarHelper;
-
-    public MainMenu(){
-        initMenuBarHelper();
-    }
-
-    private void initMenuBarHelper(){
-        menuBarHelper = new MenuBarHelper();
-    }
-
-    public JMenuBar getMenuBar(){
-        return menuBarHelper.getMenuBar();
-    }
+    //TODO:: main menu class
 
 }
