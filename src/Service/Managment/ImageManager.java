2c9adf2896ffec1073c85f9ec584cf18024adb34
Miron <hamer223@o2.pl>
2019-09-11 17:19:20 +0200

    added first methods in ImageManager class

--- src/Service/Managment/ImageManager.java
+++ src/Service/Managment/ImageManager.java
@@ -1,17 +1,42 @@
 package Service.Managment;
 
-import java.awt.*;
+import Service.Helpers.FileHelper;
+
+import javax.swing.*;
 
 public class ImageManager {
     /*
-
     //TODO:: ImageManager contains information about images and their paths, add them
-    now just colors
-
      */
-    private Color backgroundColor = new Color(160,82,45);
 
-    public Color getBackgroundColor(){
-        return backgroundColor;
+    private final String absolutePath = FileHelper.getAbsolutePath();
+
+    //metronome
+    private final static String[] metronomeIconPath = new String[2];
+    private static int metronomePos = 0;
+
+    public ImageManager(){
+        //metronome
+        setMetronomeImages();
+    }
+
+    public static ImageIcon getMetronomeIcon(){
+        changeMetronomePos();
+        return new ImageIcon(metronomeIconPath[metronomePos]);
+    }
+
+    private void setMetronomeImages(){
+        setMetronomeIconPath();
+    }
+
+    private void setMetronomeIconPath(){
+        for(int i=0;i<=1;i++){
+            metronomeIconPath[i] = absolutePath + "/src/resources/Images/metronome-" + i + ".png";
+        }
+    }
+
+    private static void changeMetronomePos(){
+        if(metronomePos==0) metronomePos=1;
+        else if(metronomePos==1) metronomePos =0;
     }
 }
